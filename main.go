5f3907c9a42ff04ccc635c84213b284a1d7f4c43
yohannes <yohannegeta9@gmail.com>
2025-04-14 20:33:07 -0600

    change to deployed link  patttern

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 
 	// main.go
 corsHandler := cors.New(cors.Options{
-    AllowedOrigins:   []string{"http://localhost:8082", "http://localhost:8081" ,"*"},
+    AllowedOrigins:   []string{"https://backendwithgo.onrender.com", "http://localhost:8081" ,"*"},
     AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
     AllowedHeaders:   []string{"Content-Type", "Authorization", "Accept"},
     ExposedHeaders:   []string{"Content-Length"},
