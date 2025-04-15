4c3fbb15173c200395cb3ff5fcd0b4b880e5c13b
yohannes <yohannegeta9@gmail.com>
2025-04-14 19:48:28 -0600

    ad change

--- database/db.go
+++ database/db.go
@@ -4,14 +4,25 @@ import (
 	"database/sql"
 	"log"
 	"os"
-
+"fmt"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func init() {
-	connStr := os.Getenv("DATABASE_URL") // Use Render's full connection string
+	// First try full connection string (for Render)
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		connStr = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		)
+	}
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
